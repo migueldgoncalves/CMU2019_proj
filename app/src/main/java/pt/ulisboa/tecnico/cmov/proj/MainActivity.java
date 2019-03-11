fdea1dddbc77f047aa76a0eb28d72e1f47d0e00e
Pmsmm <pedro8745@gmail.com>
2019-03-11 04:59:59 +0000

    Initial Screen with Sign In and Sign Up options complete

--- app/src/main/java/pt/ulisboa/tecnico/cmov/proj/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/proj/MainActivity.java
@@ -1,7 +1,10 @@
 package pt.ulisboa.tecnico.cmov.proj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE);
+
+        final Button signIn_button = findViewById(R.id.sign_in);
+        signIn_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Code for New Activity of Signing Up
+            }
+        });
+
+        final Button signUp_button = findViewById(R.id.sign_up);
+        signUp_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Code for New Activity of Signing Up
+            }
+        });
     }
 }
