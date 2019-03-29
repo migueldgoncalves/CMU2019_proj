3f7a2898c22574a26e0186e758c3365570120eac
Pmsmm <pedro8745@gmail.com>
2019-03-29 03:25:49 +0000

    Trash All arround me are familiar faces ?.?

--- app/src/main/java/pt/ulisboa/tecnico/cmov/proj/SignIn.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/proj/SignIn.java
@@ -1,18 +1,11 @@
 package pt.ulisboa.tecnico.cmov.proj;
 
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.android.Auth;
-import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.users.FullAccount;
 
 
-public abstract class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity {
 
-    private static final String ACCESS_TOKEN = "U-OMm-0dku8AAAAAAAAA_Jf-1GJ7nb7CeiaRGFGfRx2u8wbYAUWMUJzsVY7Q_EpF";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,40 +14,4 @@ public abstract class SignIn extends AppCompatActivity {
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        SharedPreferences prefs = getSharedPreferences("dropbox-sample", MODE_PRIVATE);
-        String accessToken = prefs.getString("access-token", null);
-        if (accessToken == null) {
-            accessToken = Auth.getOAuth2Token();
-            if (accessToken != null) {
-                prefs.edit().putString("access-token", accessToken).apply();
-                initAndLoadData(accessToken);
-            }
-        } else {
-            initAndLoadData(accessToken);
-        }
-
-        String uid = Auth.getUid();
-        String storedUid = prefs.getString("user-id", null);
-        if (uid != null && !uid.equals(storedUid)) {
-            prefs.edit().putString("user-id", uid).apply();
-        }
-    }
-
-    private void initAndLoadData(String accessToken) {
-        DropboxClientFactory.init(accessToken);
-        PicassoClient.init(getApplicationContext(), DropboxClientFactory.getClient());
-        loadData();
-    }
-
-    protected abstract void loadData();
-
-    protected boolean hasToken() {
-        SharedPreferences prefs = getSharedPreferences("dropbox-sample", MODE_PRIVATE);
-        String accessToken = prefs.getString("access-token", null);
-        return accessToken != null;
-    }
 }
