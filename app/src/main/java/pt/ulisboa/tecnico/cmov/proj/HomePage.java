3dd489d67ee3b0618e7d14a6e967ee3af721d609
Pmsmm <pedro8745@gmail.com>
2019-04-04 16:34:18 +0100

    Alterações à Home Page e ao Log In

--- app/src/main/java/pt/ulisboa/tecnico/cmov/proj/HomePage.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/proj/HomePage.java
@@ -140,6 +140,7 @@ public class HomePage extends DropboxActivity implements NavigationView.OnNaviga
                 username.setText(DropboxClientFactory.getClient().users().getCurrentAccount().getAccountId());
                 mail.setText(DropboxClientFactory.getClient().users().getCurrentAccount().getEmail());
             }catch (Exception e){
+
                 e.printStackTrace();
             }
         }
@@ -158,7 +159,7 @@ public class HomePage extends DropboxActivity implements NavigationView.OnNaviga
         // Set up the input
         final EditText input = new EditText(HomePage.this);
         // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_NORMAL);
         builder.setView(input);
 
         // Set up the buttons
--- app/src/main/java/pt/ulisboa/tecnico/cmov/proj/SignIn.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/proj/SignIn.java
@@ -1,16 +1,39 @@
 package pt.ulisboa.tecnico.cmov.proj;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Date;
+import java.util.HashMap;
 
 import pt.ulisboa.tecnico.cmov.proj.Data.Peer2PhotoApp;
 
 
 public class SignIn extends AppCompatActivity {
+
+    public static final String URL_BASE = "http://192.168.1.10:8080";
+    public static final String URL_SIGNUP = URL_BASE + "/signin";
+
+    Context ctx = this;
+    private RequestQueue queue = null;
+    private JSONObject httpResponse = null;
+
 
     private EditText username;
     private EditText password;
@@ -36,6 +59,63 @@ public class SignIn extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void httpRequest(String username, String password){
+        android.util.Log.d("debug", "Starting POST request to URL " + URL_SIGNUP);
+        createHTTPQueue();
+        HashMap<String, String> mapRequest = new HashMap();
+        mapRequest.put("username", username);
+        mapRequest.put("password", password);
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, URL_SIGNUP, new JSONObject(mapRequest),
+                new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject httpResponse) {
+                        try {
+                            setHTTPResponse(httpResponse);
+                            String success = httpResponse.getString("success");
+                            String error = httpResponse.getString("error");
+                            android.util.Log.d("debug", httpResponse.toString());
+                            android.util.Log.d("debug", success);
+                            android.util.Log.d("debug", error);
+                            if(!error.equals("null")) {
+                                Toast.makeText(ctx, error, Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                Toast.makeText(ctx, success, Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(ctx, SignIn.class);
+                                startActivity(intent);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        cleanHTTPResponse();
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                cleanHTTPResponse();
+                android.util.Log.d("debug", "POST error");
+            }
+        }
+        );
+        queue.add(request);
+    }
+
+    private void setHTTPResponse(JSONObject json) {
+        this.httpResponse = json;
+    }
+
+    private void cleanHTTPResponse() {
+        this.httpResponse = null;
+        android.util.Log.d("debug", "Cleaned " + new Date().getTime());
+    }
+
+    private void createHTTPQueue() {
+        if(this.queue == null) {
+            this.queue = Volley.newRequestQueue(ctx);
+        }
     }
 
     private boolean serverValidatesCredentials() {
